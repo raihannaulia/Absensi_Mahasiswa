7d95f68c28b96c3d92de6ef4b77e84e38edfedbe
raihannaulia <raikodau@gmail.com>
2023-03-28 21:07:57 +0700

    Save 1

--- app/src/main/java/org/d3if3036/assesmentmopro1/MainActivity.kt
+++ app/src/main/java/org/d3if3036/assesmentmopro1/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import org.d3if3036.assesmentmopro1.databinding.ActivityMainBinding
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var recycleView : RecyclerView
+//    private lateinit var recycleView : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +26,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        recycleView = findViewById(R.id.recycleView)
+//        recycleView = findViewById(R.id.recycleView)
+    }
+    fun ButtonCLick(view: View) {
+        val nama: EditText = findViewById(R.id.nama)
+        val tanggal: EditText = findViewById(R.id.tanggal)
+        val nim: EditText = findViewById(R.id.nim)
+        val matakuliah: EditText = findViewById(R.id.matakuliah)
+//        val present: EditText = findViewById(R.id.present)
+//        val absent: EditText = findViewById(R.id.absent)
+//        val late: EditText = findViewById(R.id.late)
+        val displayName: TextView = findViewById(R.id.displayName)
+        val displayDate: TextView = findViewById(R.id.displayDate)
+        val displayNim: TextView = findViewById(R.id.displayNim)
+        val displaySubject: TextView = findViewById(R.id.displaySubject)
+//        val displayPresent: TextView = findViewById(R.id.displayPresent)
+//        val displayAbsent: TextView = findViewById(R.id.displayAbsent)
+//        val displayLate: TextView = findViewById(R.id.displayLate)
 
-        val inputList = listOf(
-            InputDa
-        )
+        var str1:String = nama.text.toString()
+        var str2:String = tanggal.text.toString()
+        var str3:String = nim.text.toString()
+        var str4:String = matakuliah.text.toString()
+//        var str5:String = present.text.toString()
+//        var str6:String = absent.text.toString()
+//        var str7:String = late.text.toString()
 
+        displayDate.setText(str1)
+        displayName.setText(str2)
+        displayNim.setText(str3)
+        displaySubject.setText(str4)
+//        displayPresent.setText(str5)
+//        displayAbsent.setText(str6)
+//        displayLate.setText(str7)
     }
 }
--- app/src/main/java/org/d3if3036/assesmentmopro1/MainAdapter.kt
+++ app/src/main/java/org/d3if3036/assesmentmopro1/MainAdapter.kt
@@ -1,5 +1,6 @@
 package org.d3if3036.assesmentmopro1
 
+import android.app.Person
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,32 +8,26 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import org.d3if3036.assesmentmopro1.databinding.ActivityMainBinding
-
-class MainAdapter(private val inputList: List<ListNama>) :
-    RecyclerView.Adapter<MainAdapter.ViewHolder>() {
-
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-    }
-
-    override fun onBindViewHolder(holder: MainAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val textView: TextView = itemView.findViewById(R.id.textView)
-
-        fun bind(listNama: ListNama) {
-            textView.text = listNama.nama
-
-
-        }
-
-    }
-}
+//
+//class MainAdapter(private val inputList: List<ListNama>) :
+//    RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+//    private var oldPersonList = emptyList<Person>()
+//
+//    class MyViewHolder(val binding:)
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainAdapter.ViewHolder {
+//        val view = LayoutInflater.from(parent.context)
+//    }
+//
+//    override fun onBindViewHolder(holder: MainAdapter.ViewHolder, position: Int) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getItemCount(): Int {
+//        TODO("Not yet implemented")
+//    }
+//
+//
+//
+//    }
+//}
